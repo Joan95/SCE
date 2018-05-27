86d5054785be4403a1a8b0448a0802155c7bc688
Joan95 <joancanals95@gmail.com>
2018-05-27 16:14:34 +0200

    last commit

--- Shop/app/controllers/pages_controller.rb
+++ Shop/app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
 	def home
 		session[:products] ||= []
+
+		@response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/')
+		Rails.logger.debug("My object: #{@response.inspect}")
 	end
 end

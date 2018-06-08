3253f583be9783f9f9decb0ecf609b8b9bb063ff
Joan95 <joancanals95@gmail.com>
2018-06-08 18:10:57 +0200

    Registration bug solved

--- BricksShop/app/controllers/registrations_controller.rb
+++ BricksShop/app/controllers/registrations_controller.rb
@@ -1,12 +1,14 @@
-class RegistrationsController < Devise::RegistrationsController
-	
-	private 
+class RegistrationsController < Devise::RegistrationsController 
 
 	def sign_up_params
 		params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation, :email, :address, :mobile)
 	end
 
 	def account_update_params
-		params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation, :email, :address, :mobile)
+		params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation, :email, :address, :mobile, :current_password)
+	end
+
+	def edit
+
 	end
 end
